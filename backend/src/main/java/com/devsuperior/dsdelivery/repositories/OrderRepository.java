306ca97d81fe7eedc12d1e977b0632c215f1a073
Paulo Dutra <dutra.phs@gmail.com>
2021-01-10 17:28:50 -0300

    orders with jpql to bring older pendings

---
+++ backend/src/main/java/com/devsuperior/dsdelivery/repositories/OrderRepository.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dsdelivery.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.dsdelivery.entites.Order;
+
+
+ public interface OrderRepository extends JpaRepository<Order, Long> {		 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+     @Query("SELECT DISTINCT obj from Order obj " +
+            "JOIN FETCH obj.products " +
+            " WHERE obj.status = 0 ORDER BY obj.moment ASC")
+
+     List<Order> findOrdersWithProducts();
+
+	}
+}
